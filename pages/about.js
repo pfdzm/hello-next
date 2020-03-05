2e80f788ce1609eefa0157bae78d054d4bc47e82
Pablo Fernandez <pfdzm@users.noreply.github.com>
2020-03-05 08:19:56 +0100

    first commit with progress made on next.js tutorial

---
+++ pages/about.js
@@ -0,0 +1,8 @@
+import Layout from "../components/MyLayout";
+export default function About() {
+  return (
+    <Layout>
+      <p>This is the about page.</p>
+    </Layout>
+  );
+}
---
+++ pages/index.js
@@ -0,0 +1,24 @@
+import Layout from "../components/MyLayout";
+import Link from "next/link";
+
+const PostLink = props => (
+  <li>
+    <Link href="/p/[id]" as={`/p/${props.id}`}>
+      <a>{props.id}</a>
+    </Link>
+  </li>
+);
+export default function Index() {
+  return (
+    <div>
+      <Layout>
+        <h1>My Blog</h1>
+        <ul>
+          <PostLink id="hello-nextjs" />
+          <PostLink id="learn-nextjs" />
+          <PostLink id="deploy-nextjs" />
+        </ul>
+      </Layout>
+    </div>
+  );
+}
